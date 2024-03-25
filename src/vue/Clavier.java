cc5baceb58baf93a44d32a918a5832367fdcf4d0
Houston <23219631+HoustonLFB@users.noreply.github.com>
2024-03-25 15:05:36 +0100

    TD5 4) TD TERMINEEEE DM TERMINEEEE

--- src/vue/Clavier.java
+++ src/vue/Clavier.java
@@ -1,12 +1,19 @@
 package vue;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Clavier {
     private static Scanner scanner = new Scanner(System.in);
 
     public static int entrerClavierInt() {
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Vous devez entrer un entier");
+            scanner.next(); // to consume the invalid token
+            return entrerClavierInt();
+        }
     }
 
     public static String entrerClavierString() {
